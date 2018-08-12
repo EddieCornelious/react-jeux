904cbc28b9fe3810a52592611ed5f6ddf27905c8
Eddie Cornelious <eulerissmart@gmail.com>
2018-08-12 00:06:57 +0000

    eh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import {createStore} from 'jeux';
-console.log(createStore);
-export {};
+
+import React, {Component} from 'react';
+
+class Ele extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { lol: [React]};
+  }
+  render() {
+    return <h1>lol</h1>;
+  }
+}
+
+export {Ele};
