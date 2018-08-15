0df21b21675f335eb53780971c2d7872dbfbff47
Eddie Cornelious <eulerissmart@gmail.com>
2018-08-15 08:10:19 +0000

    more test setup

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,37 +6,41 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 import { mount } from 'enzyme';
-import React, {Component, createElement} from 'react';
-import {createStore } from 'jeux';
+import React, { Component, createElement } from 'react';
+import { createStore } from 'jeux';
 import sinon from 'sinon';
 
 chai.expect();
 
 const expect = chai.expect;
 let component = null;
-afterEach(()=>{
-    component.unmount()
-})
-class Counter extends Component{
-    constructor(props) {
-        super(props);
-    }
-    
-    render(){
-        return (
-            <h1>ji</h1>
-            
-            )
-    }
-    
+
+function counterReducer(state, action) {}
+
+function sumReducer(state, action) {}
+const reducers = {
+  counter: counterReducer,
+  sum: sumReducer
+};
+const store = createStore(reducers);
+
+afterEach(() => {
+  component.unmount();
+});
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>ji</h1>;
+  }
 }
 
 describe('connect', () => {
- 
- it("is a function that returns a react component", ()=>{
-     component = mount(<Counter/>)
-     expect(component.html()).to.equal('<h1>ji</h1>');
- })  
-    
-    
+  it('is a function that returns a react component', () => {
+    component = mount(<Counter />);
+    expect(component.html()).to.equal('<h1>ji</h1>');
+  });
 });
