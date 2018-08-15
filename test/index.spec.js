21071b7023113511b605c846d1220f945823a47c
Eddie Cornelious <eulerissmart@gmail.com>
2018-08-15 07:58:13 +0000

    added test file pre setup

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,42 @@
 /* global describe, it, before */
 
 import chai from 'chai';
-import {Ele} from '../src/index.js';
+import { connect } from '../src/connect.js';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+import { mount } from 'enzyme';
+import React, {Component, createElement} from 'react';
+import {createStore } from 'jeux';
+import sinon from 'sinon';
 
 chai.expect();
 
 const expect = chai.expect;
+let component = null;
+afterEach(()=>{
+    component.unmount()
+})
+class Counter extends Component{
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return (
+            <h1>ji</h1>
+            
+            )
+    }
+    
+}
 
-let lib;
-
-describe('Given an instance of my Cat library', () => {
+describe('connect', () => {
  
+ it("is a function that returns a react component", ()=>{
+     component = mount(<Counter/>)
+     expect(component.html()).to.equal('<h1>ji</h1>');
+ })  
+    
+    
 });
