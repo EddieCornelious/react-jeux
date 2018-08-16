8eff38ceb53088091b81dbe994c75ae34f0bdefe
Eddie Cornelious <eulerissmart@gmail.com>
2018-08-16 10:41:08 +0000

    changed library name

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const config = {
   output: {
     path: __dirname + '/lib',
     filename: outputFile,
-    library: "reactjeux",
+    library: "ReactJeux",
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
